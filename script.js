27dfbaa55b90bda95d9f5f0dbd262483b7973bbb
vivian young <vivian.young.2000@gmail.com>
2019-12-21 16:42:54 -0800

    Remove extra button

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
+  console.log("opening menu");
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("left").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
+  console.log("closing menu");
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("left").style.marginLeft = "0";
 }
 
+function toggleNav(bool) {
+  if (bool) {
+    openNav();
+  } else {
+    closeNav();
+  }
+}
+
 $(document).ready( () => {
     console.log("hello");
+
+    let menuShow = false;
 
     $("#aboutme").click( () => {
         console.log("clicked about me");
@@ -30,8 +42,9 @@ $(document).ready( () => {
 
   $(".menu-icon-container").click( () => {
       console.log("clicked menu");
+      menuShow = !menuShow;
       $(".menu-icon-container").toggleClass("change");
-      // $(".left").css("animation", "menu-show 2s ease");
-})
+      toggleNav(menuShow);
+  })
 
 });
