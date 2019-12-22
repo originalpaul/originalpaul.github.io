fb2142d0cc33ac7e4302010427f2b0b07d4b6f4d
vivian young <vivian.young.2000@gmail.com>
2019-12-21 16:27:58 -0800

    Add new changes to menu and re-center

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("left").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("left").style.marginLeft = "0";
+}
+
 $(document).ready( () => {
     console.log("hello");
 
